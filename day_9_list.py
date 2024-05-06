31b2ec1a3bf0e2a916d3dae5dc89cbec3f6d703b
priyaaa2345 <apriyadharshiniarumugam@gmail.com>
2024-05-06 12:13:20 +0530

    change

--- day_9_list.py
+++ day_9_list.py
@@ -10,8 +10,19 @@ list=[1,2,3,4,44,55,33]
 # list.insert(4,46)    #to insert at particular index
 # print(list)
 
-groc=[100,200,300,400]    # to add 2 list into one
+# groc=[100,200,300,400]    # to add 2 list into one
 
-groce=[450,333,454]
-output=[groc+groce]
-print(output)
+# groce=[450,333,454]
+# output=[groc+groce]
+# print(output)
+
+# output.pop()                #to delete
+
+pri=[10,20,30]
+pri_copy=pri
+pric1=[10,20,30]
+pri_copy.append(600)
+pri.append(700)
+pric1.append(800)
+
+print(pri,pri_copy,pric1)
