e7399cc0ba3f9b9c0726ee8e45a8acd11773050d
priyaaa2345 <apriyadharshiniarumugam@gmail.com>
2024-05-08 18:14:54 +0530

    change

--- day_10_cool.py
+++ day_10_cool.py
@@ -16,22 +16,22 @@ from datetime import datetime
 # print(datetime.now())
 
 
-# calculate age
-def Calculate_age(year):
-    try:
-        if 2024 > year:
-            age = 2024 - year
+# # calculate age
+# def Calculate_age(year):
+#     try:
+#         if 2024 > year:
+#             age = 2024 - year
 
-    except ValueError:
-        print("year should be int")
-    else:
-        print("birth year cant be greater than 2024")
+#     except ValueError:
+#         print("year should be int")
+#     else:
+#         print("birth year cant be greater than 2024")
 
-    print(f"your age is {age}")
+#     print(f"your age is {age}")
 
 
-year = int(input("enter your year of birth"))
-Calculate_age(year)
+# year = int(input("enter your year of birth"))
+# Calculate_age(year)
 
 
 # def Calculate_age():
